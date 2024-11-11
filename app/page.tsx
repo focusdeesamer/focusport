c72a65bab0a86b14f43f87bac825160a0c560561
focusdeesamer <ggzolofc@gmail.com>
2024-11-11 14:21:49 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -25,9 +25,52 @@ export default function Page() {
         Portfolio, made simple!
       </h1>
       <div className="prose prose-neutral dark:prose-invert">
-        <p>Welcome to my portfolio site. Here you can find information about my projects and skills.</p>
+        <p>
+          A clean, fast, and lightweight portfolio template built with Next.js,
+          Vercel, and Tailwind CSS for optimal performance.
+        </p>
+        <p>
+          Nextfolio includes all the essentials for a stunning portfolio: SEO,
+          MDX support, RSS, Atom, & JSON feeds, analytics, tweet & YouTube
+          embeds, KaTeX integration, and{" "}
+          <a
+            target="_blank"
+            href="https://github.com/1msirius/Nextfolio?tab=readme-ov-file#features"
+          >
+            more
+          </a>
+          .
+        </p>
+        <p>
+          Nextfolio is{" "}
+          <a href={socialLinks.github} target="_blank">
+            open-source
+          </a>{" "}
+          and fully customizable, making it easy to add more features.
+        </p>
+        <p>
+          <a
+            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2F1msirius%2FNextfolio"
+            target="_blank"
+          >
+            Deploy
+          </a>{" "}
+          your Nextfolio site with Vercel in minutes and follow the set up
+          instructions in the{" "}
+          <a href="/blog/getting-started">Getting Started</a> post.
+        </p>
+        <p>
+          Built and maintained by{" "}
+          <a href="https://imsirius.xyz/" target="_blank">
+            Sirius
+          </a>
+          .
+        </p>
+        <p>
+          Welcome to my portfolio site. Here you can find information about my
+          projects and skills.
+        </p>
       </div>
     </section>
   );
 }
-
